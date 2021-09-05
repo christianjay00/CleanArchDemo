c4608f5d19e48ce198a74e631965db208aa140ce
Christian Jay Feliciano <tian@bizbox.ph>
2021-09-05 19:53:25 +0800

    Course Service Create Functionality, Update courseViewModel

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -10,6 +10,6 @@ namespace CleanArch.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
-
+        void Create(CourseViewModel courseViewModel);
     }
 }
