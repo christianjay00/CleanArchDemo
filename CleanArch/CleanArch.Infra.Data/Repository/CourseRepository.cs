312c4cbcde76ae6f0313c2cf637e82e7b0d46571
Christian Jay Feliciano <tian@bizbox.ph>
2021-09-05 11:37:19 +0800

    Commit Implementation of Service and Repository Get Courses methos

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Domain.Models;
+using CleanArch.Domain.Models.Interfaces;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _ctx;
+
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            //throw new NotImplementedException();
+            return _ctx.Courses;
+        }
+    }
+}
